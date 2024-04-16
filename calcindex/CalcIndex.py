ffc6df6c589404cfd8ac63ea13ffd06ab84c75d1
Nilesh Indore <nindore@linius.com>
2024-04-16 23:36:17 +0530

    update

--- calcindex/CalcIndex.py
+++ calcindex/CalcIndex.py
@@ -5,29 +5,28 @@ import datetime
 import os
 
 mysqlHost = os.environ['MYSQL_HOST']
-redisHost = os.environ['REDIS_HOST']
+mysqlUser = os.environ['MYSQL_USER']
+mysqlPassword = os.environ['MYSQL_PASSWORD']
+mysqlDbName = os.environ['MYSQL_DB']
 
 while True:
-    mydb = mysql.connector.connect(host=mysqlHost,user='root',passwd='root',db='BHARATINDEX')   
+    mydb = mysql.connector.connect(host=mysqlHost,user=mysqlUser,passwd=mysqlPassword,db=mysqlDbName)   
     mycursor = mydb.cursor()
-    query = "select s_name from s_detail"
+    query = "select s_id from s_detail"
     mycursor.execute(query)
     indexValue = 0
-    for i in mycursor:
-        r = redis.Redis(host=redisHost)
-        print(i[0])
-        indexValue += int(r.get(i[0]))
-    print(indexValue)
-    query = "select i_id from i_detail where i_name='BHARATINDEX'"
+    for stockid in mycursor:
+        query = "select s_price from s_price where s_id="+stockid+" ORDER BY s_ttime DESC LIMIT 1"
+        indexValue += mycursor.execute(query)
+    query = "select i_id from i_detail where i_name="+mysqlDbName
     mycursor.execute(query)
     i_id = mycursor.fetchone()[0]
-    print(type(i_id))
     tick_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #tick_time = datetime.datetime.now()
-    print(tick_time)
-    #insertQuery = "insert into i_price VALUES ({},{},{})".format(i_id,tick_time,indexValue)
+    print("tick_time = " + tick_time)
+    print("index value = " + indexValue)
     mycursor.execute("""insert into i_price (i_id,i_ttime,i_price) values (%s,%s,%s)""",(i_id,tick_time,indexValue))
     mydb.commit()
     mycursor.close()
     mydb.close()
+    print("Pushed to DB")
     time.sleep(5)
