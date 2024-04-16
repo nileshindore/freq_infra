41bd779a00d795f7c22376425805fa0a229cb1ae
Nilesh Indore <nindore@linius.com>
2024-04-17 01:01:40 +0530

    update

--- calcindex/CalcIndex.py
+++ calcindex/CalcIndex.py
@@ -1,4 +1,3 @@
-import redis
 import mysql.connector
 import time
 import datetime
@@ -14,21 +13,30 @@ while True:
     mycursor = mydb.cursor()
     query = "select s_id from s_detail"
     mycursor.execute(query)
-    indexValue = 0
-    for stockid in mycursor:
-        cursor = mydb.cursor()
-        query = "select s_price from s_price where s_id="+str(stockid)+" ORDER BY s_ttime DESC LIMIT 1"
-        cursor.execute(query)
-        indexValue += int(cursor)
-    query = "select i_id from i_detail where i_name="+mysqlDbName
-    mycursor.execute(query)
-    i_id = mycursor.fetchone()[0]
-    tick_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print("tick_time = " + tick_time)
-    print("index value = " + indexValue)
-    mycursor.execute("""insert into i_price (i_id,i_ttime,i_price) values (%s,%s,%s)""",(i_id,tick_time,indexValue))
-    mydb.commit()
+    ids = [] 
+    for id in mycursor.fetchall():
+        ids.append(id[0])
+    # print(ids)
     mycursor.close()
-    mydb.close()
-    print("Pushed to DB")
-    time.sleep(5)
+    indexValue = 0
+    for stockid in ids:
+        # print(stockid)
+        # print(type(stockid))
+        mycursor = mydb.cursor(buffered=True)
+        query = "select s_price from s_price where s_id = `%s` ORDER BY s_ttime DESC LIMIT 1" % stockid
+        mycursor.execute(query)
+        print(mycursor)
+        # indexValue += int(cursor)
+    # print(indexValue)
+    # query = "select i_id from i_detail where i_name="+mysqlDbName
+    # mycursor.execute(query)
+    # i_id = mycursor.fetchone()[0]
+    # tick_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # print("tick_time = " + tick_time)
+    # print("index value = " + indexValue)
+    # mycursor.execute("""insert into i_price (i_id,i_ttime,i_price) values (%s,%s,%s)""",(i_id,tick_time,indexValue))
+    # mydb.commit()
+    # mycursor.close()
+    # mydb.close()
+    # print("Pushed to DB")
+    # time.sleep(5)
