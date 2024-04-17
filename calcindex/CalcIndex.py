ff97fcd75b72891238d040e25c2a8af6b0864822
Nilesh Indore <nindore@linius.com>
2024-04-17 18:48:29 +0530

    update

--- calcindex/CalcIndex.py
+++ calcindex/CalcIndex.py
@@ -16,27 +16,25 @@ while True:
     ids = [] 
     for id in mycursor.fetchall():
         ids.append(id[0])
-    # print(ids)
     mycursor.close()
     indexValue = 0
     for stockid in ids:
-        # print(stockid)
-        # print(type(stockid))
         mycursor = mydb.cursor(buffered=True)
-        query = "select s_price from s_price where s_id = `%s` ORDER BY s_ttime DESC LIMIT 1" % stockid
+        query = "select s_price from s_price where s_id = '%s' ORDER BY s_ttime DESC LIMIT 1" % stockid
         mycursor.execute(query)
-        print(mycursor)
-        # indexValue += int(cursor)
-    # print(indexValue)
-    # query = "select i_id from i_detail where i_name="+mysqlDbName
-    # mycursor.execute(query)
-    # i_id = mycursor.fetchone()[0]
-    # tick_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # print("tick_time = " + tick_time)
-    # print("index value = " + indexValue)
-    # mycursor.execute("""insert into i_price (i_id,i_ttime,i_price) values (%s,%s,%s)""",(i_id,tick_time,indexValue))
-    # mydb.commit()
-    # mycursor.close()
-    # mydb.close()
-    # print("Pushed to DB")
-    # time.sleep(5)
+        indexValue += mycursor.fetchall()[0][0]
+        mycursor.close()
+    print(indexValue)
+    query = "select i_id from i_detail where i_name = '%s'" % mysqlDbName
+    mycursor = mydb.cursor(buffered=True)
+    mycursor.execute(query)
+    i_id = mycursor.fetchone()[0]
+    tick_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("tick_time = " + tick_time)
+    print("index value = " + str(indexValue))
+    mycursor.execute("""insert into i_price (i_id,i_ttime,i_price) values (%s,%s,%s)""",(i_id,tick_time,indexValue))
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
+    print("Pushed to DB")
+    time.sleep(5)
