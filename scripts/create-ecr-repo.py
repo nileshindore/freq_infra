77ea3bf7de898f428b5fcb06ed9622d686abc0d9
Nilesh Indore <nindore@linius.com>
2024-04-20 00:45:36 +0530

    update

--- scripts/create-ecr-repo.py
+++ scripts/create-ecr-repo.py
@@ -7,6 +7,6 @@ parser.add_argument('--name', help='Repository Name')
 args = parser.parse_args()
 if args.name:
     client = boto3.client("ecr",region_name='us-east-2update')
-    client.create_repository(args.name)
+    client.create_repository(repositoryName=args.name)
 else:
     print("Repository name need to provide.")
